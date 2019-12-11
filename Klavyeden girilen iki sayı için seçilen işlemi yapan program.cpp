2516cb7c984d2f79aec3f03fe091f3f32663fa05
Onur10 <onur1903sahin@hotmail.com>
2019-12-11 15:43:01 +0300

    Add files via upload

---
+++ Klavyeden girilen iki sayı için seçilen işlemi yapan program.cpp
@@ -0,0 +1,45 @@
+﻿// ConsoleApplication3.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	int sayi1, sayi2, sonuc;
+	char islem;
+
+	cout << "birinci sayiyi girin : " << endl;
+	cin >> sayi1;
+	cout << "ikinci sayiyi girin : " << endl;
+	cin >> sayi2;
+	cout << "islem seciniz + - * / " << endl;
+	cin >> islem;
+
+	if (islem =='+')
+	{
+		sonuc = sayi1 + sayi2;
+		cout << "Sonuc : " << sonuc << endl;
+	}
+	else if (islem=='-')
+	{
+		sonuc = sayi1 - sayi2;
+		cout << "Sonuc : " << sonuc << endl;
+	}
+	else if (islem=='*')
+	{
+		sonuc = sayi1 * sayi2;
+		cout << "Sonuc : " << sonuc << endl;
+	}
+	else
+	{
+		sonuc = sayi1 / sayi2;
+		cout << "Sonuc : " << sonuc << endl;
+	}
+	system("pause");
+	return 0;
+}
+
